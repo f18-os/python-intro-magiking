4cf958965d424d610c6d54a24c8b1d7d941cfc4e
Jake Lasley <jlasley@miners.utep.edu>
2018-09-03 19:20:46 -0600

    Add Makefile and really polished version.
    coolerWordCount.py is a version of wordCount.py that uses a really
    nice python builtin called a Counter. I didn't want you guys to think
    I missed the point of the exercise if I used one of these.

---
+++ Makefile
@@ -0,0 +1,13 @@
+test:
+	echo 'Testing wordCount.py'
+	echo 'speech...'
+	python3 wordCountTest.py speech.txt speechOut.txt speechKey.txt
+	echo 'declaration...'
+	python3 wordCountTest.py declaration.txt declarationOut.txt declarationKey.txt
+
+cool-test:
+	echo 'Testing coolerWordCount.py'
+	echo 'speech...'
+	python3 coolerWordCountTest.py speech.txt speechOutCool.txt speechKey.txt
+	echo 'declaration...'
+	python3 coolerWordCountTest.py declaration.txt declarationOutCool.txt declarationKey.txt
---
+++ coolerWordCount.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+import os
+from collections import Counter # For easy counting
+
+# input and output files
+if len(sys.argv) is not 3:
+    print("Correct usage: coolerWordCount.py <input text file> <output text file>")
+    exit()
+
+inputFname = sys.argv[1]
+outputFname = sys.argv[2]
+
+#check that input exists
+if not os.path.exists(inputFname):
+    print("%s does not exist. Exiting" % inputFname)
+    exit()
+
+# words "dictionary"
+count = Counter()
+
+# open input file and read. Insert words into dictionary line by line
+with open(inputFname, 'r') as inputFile:
+    words = re.findall('\w+', inputFile.read().lower())
+    for word in words:
+        count[word.lower()] += 1
+
+# write dictionary to output file
+with open(outputFname, 'w') as outputFile:
+    for word in sorted(count):
+        outputFile.write("%s\t%d\n" % (word, count[word]))
