5f8d8f1cc6dd1d7cefd93d680ebdda0fe360a1b0
Jake Lasley <jlasley@miners.utep.edu>
2018-09-14 13:21:46 -0600

    Changed run_ to pass fds to exec_
    Gettting bad fd errors from the execve()'d programs.
    run_() used to pass the filepath of the files to be redirected,
    and the child would open the file and set its file descriptor.
    Now run_() opens the filepath, sets the new fd to inheritable,
    and passes it to exec_

--- shell/shelly.py
+++ shell/shelly.py
@@ -7,46 +7,62 @@ import re
 pid = os.getpid()
 
 def read_():
+    '''
+    Handle user input
+    '''
     # get the command
-    args = re.split(' ', input('shelly $ '))
+    args = re.split(' ', str.strip(input('shelly $ ')))
     if args[0] == 'exit':
         sys.exit(0)
 
-    print(args)
+    # print(args)
 
     # check for redirection
     in_ = None
     out = None
-    new_in = None
-    new_out = None
+    new_in_fpath = None
+    new_out_fpath = None
+    new_in_fd = None
+    new_out_fd = None
     try:
         index = 0
         while index < len(args):
             arg = args[index]
             if arg == '<':
                 in_ = index
-                new_in = args[index + 1]
+                new_in_fpath = args[index + 1]
                 del args[index]
                 del args[index]
             elif arg == '>':
                 out = index
-                new_out = args[index + 1]
+                new_out_fpath = args[index + 1]
                 del args[index]
                 del args[index]
             else:
                 index += 1
         if in_:
-            print("new_in: %s at index: %d " % (new_in, in_))
+            new_in_fd = os.open(new_in_fpath, os.O_RDONLY)
+            # new_in_fd = open(new_in_fpath, 'r').fileno()
+            os.set_inheritable(new_in_fd, True)
+            print("fpath: %s at index: %d fd: %d" % (new_in_fpath, in_, new_in_fd))
         if out:
-            print("new_out: %s at index: %d " % (new_out, out))
-        # TODO: assuming that input comes before output 
+            new_out_fd = os.open(new_out_fpath, os.O_WRONLY)
+            # new_out_fd = open(new_out_fpath, 'w').fileno()
+            os.set_inheritable(new_out_fd, True)
+            print("fpath: %s at index: %d fd: %d" % (new_out_fpath, out, new_out_fd))
         print(args)
-        exec_(args=args, in_redir=new_in, out_redir=new_out)
+        exec_(args=args, in_fd=new_in_fd, out_fd=new_out_fd)
     except IndexError:
         print("Your redirection looks a little funny, try again")
     
 
-def exec_(args=[], in_redir=None, out_redir=None):
+def exec_(args=[], in_fd=None, out_fd=None):
+    '''
+    Forks a child process and execs args.
+        args: list of agruments to be passed to os.execve()
+        in_fd: integer of new input file descriptor
+        out_fd: integer of new output file descriptor
+    '''
     os.write(1, ("Command read...About to fork (pid:%d)\n" % pid).encode())
 
     rc = os.fork()
@@ -58,18 +74,23 @@ def exec_(args=[], in_redir=None, out_redir=None):
     elif rc == 0:                   # child
         os.write(1, ("Child: My pid=%d.  Parent's pid=%d\n" % 
                     (os.getpid(), pid)).encode())
-        if in_redir:
-            os.close(0)                 # redirect child's stdout
-            sys.stdin = open(in_redir, "r")
-            fd = sys.stdin.fileno() # os.open(in_redir, os.O_CREAT)
-            os.set_inheritable(fd, True)
-            os.write(2, ("Child: opened fd=%d for reading\n" % fd).encode())
-        if out_redir:
-            os.close(1)                 # redirect child's stdout
-            sys.stdout = open(out_redir, "w")
-            fd = sys.stdout.fileno() # os.open(out_redir, os.O_CREAT)
-            os.set_inheritable(fd, True)
-            os.write(2, ("Child: opened fd=%d for writing\n" % fd).encode())
+
+        # change stdin/out fds if given
+        if in_fd:
+            os.write(2, ("Child: given in_fd: %d\n" % in_fd).encode())
+            os.close(0)
+            os.dup(in_fd)
+            os.close(in_fd)
+            fd = sys.stdin.fileno()
+            os.write(2, ("Child: using fd=%d for reading\n" % fd).encode())
+        if out_fd:
+            os.write(2, ("Child: given out_fd: %d\n" % out_fd).encode())
+            os.close(1)
+            os.dup(out_fd)
+            os.close(out_fd)
+            fd = sys.stdout.fileno()
+            os.write(2, ("Child: using fd=%d for writing\n" % fd).encode())
+
         for dir in re.split(":", os.environ['PATH']): # try each directory in the path
             program = "%s/%s" % (dir, args[0])
             os.write(1, ("Child:  ...trying to exec %s\n" % program).encode())
