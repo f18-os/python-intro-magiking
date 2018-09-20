0ea6b87d4bdd590bc256059e983a3577b31e8fd5
Jake Lasley <jlasley@miners.utep.edu>
2018-09-19 22:06:18 -0600

    Update readme and add collaborations.md

---
+++ shell/collaborations.md
@@ -0,0 +1,13 @@
+# Collaborations
+
+We didn't really talk specifics about these, but just gonna throw it in here to be safe.
+
+## Phillip Hassoun
+
+- Idioms for changing stdin/stdout
+- Putting the code that forks a child into a separate method
+
+## Andres Llausas
+
+- Idioms for changing stdin/stdout
+- Remembering to close file descriptors
--- shell/shelly.py
+++ shell/shelly.py
@@ -25,15 +25,12 @@ def read_():
         prompt = os.environ['PS1']
     input_ = input(prompt)
     input_ = str.strip(input_)
-    print("input: %s" % input_)
     if not input_:
         return
     if '|' in input_: # TIME TO PIPE!
         commands = re.split('\|', str.strip(input_))  # TODO rename commands to chain? chain and link?
-        # print("read_() commands: %s" % commands)
         pipefds = (None, None)
         for index, command in enumerate(commands):
-            # print("read_() command: %s" % command)
             pread = None
             pwrite = None
             # do I need to read from a pipe? (was one made before me?)
@@ -45,15 +42,12 @@ def read_():
                for f in pipefds:
                    os.set_inheritable(f, True)
                    close_these_fds.append(f)
-               print("pipefds: %s, %s" % (pipefds[0], pipefds[1]))
+               print("pipefds: %s, %s" % (pipefds[0], pipefds[1])) # do some more testing to be sure, but take this out
                pwrite = pipefds[1]
             args = re.split(' ', str.strip(command))
-            # print("read_() args: %s" % args)
             if args[0] == 'exit':
                 sys.exit(0)
             p = prep_subprocess(args=args, pipe_read=pread, pipe_write=pwrite)
-            # print("read_() create p, id of p: %d" % id(p))
-            # print(p)
             if not p:
                 paranoid_pipefd_close()
                 return
@@ -63,18 +57,13 @@ def read_():
         if args[0] == 'exit':
             sys.exit(0)
         p = prep_subprocess(args=args)
-        # print("read_() created p, id of p: %d" % id(p))
-        # print(p)
         if not p:
             paranoid_pipefd_close()
             return
         processes.append(p)
 
-    print("before execing. close_these_fds: %s" % close_these_fds)
     # start execing stuff!
     for index, p in enumerate(processes):
-        # print("read_() exec p, id of p: %d" % id(p))
-        # print(p)
         if index == len(processes) - 1: # when last one, wait
             exec_(args=p.args, in_fd=p.input, out_fd=p.output)
         else:
@@ -82,27 +71,22 @@ def read_():
 
     paranoid_pipefd_close()
  
-def paranoid_pipefd_close():
+def paranoid_pipefd_close(): # should only be called from the parent
+    ''' Close all fds in 'close_these_fds list.'''
     global close_these_fds
     for fd in close_these_fds:
         try:
-            print("parent read_() closing fd: %d" % fd)
             os.close(fd)
-        except OSError:
-            print("looks like this one was already closed")
+        except OSError: # already closed
             pass
 
 def prep_subprocess(args=[], pipe_read=None, pipe_write=None):
     ''' Return a process object that read can execute.'''
     p = Process()
-    # print("prep: id of p: %d" % id(p))
-    # print("prep: given args: %s" % args)
-    # print("prep: id of p.args %d" % id(p.args))
     new_in_fpath = None
     new_out_fpath = None
     
-    # print("prep: p before try %s" % p)
-    try:
+    try: # check for < > file redirection
         index = 0
         while index < len(args):
             arg = args[index]
@@ -119,27 +103,23 @@ def prep_subprocess(args=[], pipe_read=None, pipe_write=None):
             else:
                 p.args.append(arg)
                 index += 1
-
-        if new_in_fpath:
-            p.input= os.open(new_in_fpath, os.O_RDONLY)
-            os.set_inheritable(p.input, True)
-            print("fpath: %s fd: %d" % (new_in_fpath, p.input))
-        else:
-            p.input = pipe_read
-
-        if new_out_fpath:
-            p.output = os.open(new_out_fpath, os.O_WRONLY | os.O_CREAT)
-            os.set_inheritable(p.output, True)
-            print("fpath: %s fd: %d" % (new_out_fpath, p.output))
-        else:
-            p.output = pipe_write
  
     except IndexError:
-        # TODO check to make sure nothing tries to run using this p if there is an error
-        print("Your redirection looks a little funny, try again")
+        os.write(2, ("Shelly: Unexpected redirection %s \n" % args).encode())
         return None
 
-    # print("prep: returning %s" % p)
+    if new_in_fpath:
+        p.input= os.open(new_in_fpath, os.O_RDONLY)
+        os.set_inheritable(p.input, True)
+    else:
+        p.input = pipe_read
+
+    if new_out_fpath:
+        p.output = os.open(new_out_fpath, os.O_WRONLY | os.O_CREAT)
+        os.set_inheritable(p.output, True)
+    else:
+        p.output = pipe_write
+
     return p
 
 
@@ -152,7 +132,6 @@ def exec_(args=[], in_fd=None, out_fd=None, parent_wait=True):
         parent_wait: if True, then parent waits for child process to terminate
     '''
     global close_these_fds
-    os.write(1, ("Command %s read wait= %s \nAbout to fork (pid:%d)\n" % (args[0], parent_wait, pid)).encode())
 
     rc = os.fork()
 
@@ -162,33 +141,21 @@ def exec_(args=[], in_fd=None, out_fd=None, parent_wait=True):
 
     elif rc == 0:                   # child
         child_pid = os.getpid()
-        os.write(1, ("Child: My pid=%d.  Parent's pid=%d\n" % 
-                    (child_pid, pid)).encode())
 
         # change stdin/out fds if given
         if in_fd:
-            os.write(2, ("Child: pid=%d given in_fd: %d\n" %
-                         (child_pid, in_fd)).encode())
             os.close(0)
             fd = os.dup(in_fd)
             os.set_inheritable(fd, True)
             os.close(in_fd)
-            os.write(2, ("Child: pid=%d stdin 0 -> %d\n" %
-                         (child_pid, in_fd)).encode())
         if out_fd:
-            os.write(2, ("Child: pid=%d given out_fd: %d\n" %
-                         (child_pid, out_fd)).encode())
             os.close(1)
             fd = os.dup(out_fd)
             os.set_inheritable(fd, True)
             os.close(out_fd)
-            os.write(2, ("Child: pid=%d stdout 1 -> %d\n" %
-                         (child_pid, out_fd)).encode())
 
         for fd in close_these_fds:
             try:
-                os.write(2, ("Child: pid=%d " % (child_pid)).encode())
-                os.write(2, ("child: closing fd: %d \n" % fd).encode())
                 os.close(fd)
             except OSError:
                 pass
@@ -196,8 +163,6 @@ def exec_(args=[], in_fd=None, out_fd=None, parent_wait=True):
         for dir in re.split(":", os.environ['PATH']):
             # try each directory in the path
             program = "%s/%s" % (dir, args[0])
-            os.write(1, ("Child: pid=%d  ...trying to exec %s\n" %
-                         (child_pid, program)).encode())
             try:
                 os.execve(program, args, os.environ) # try to exec program
             except FileNotFoundError:             # ...expected
@@ -208,30 +173,18 @@ def exec_(args=[], in_fd=None, out_fd=None, parent_wait=True):
         sys.exit(1)                 # terminate with error
 
     else:                           # parent (forked ok)
-        os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" % 
-                    (pid, rc)).encode())
         if in_fd:
-            os.write(1, ("Parent: My pid=%d " % 
-                        (pid)).encode())
-            os.write(1, ("closing fd: %d \n" % in_fd).encode())
             os.close(in_fd)
         if out_fd:
-            os.write(1, ("Parent: My pid=%d " % 
-                        (pid)).encode())
-            os.write(1, ("closing fd: %d \n" % out_fd).encode())
             os.close(out_fd)
 
         while parent_wait:
-            os.write(1, ("Waiting Parent: My pid=%d.  Child's pid=%d\n" % 
-                        (pid, rc)).encode())
             try:
                 childPidCode = os.wait()
-                os.write(1, ("Parent: Child %d terminated with exit code %d\n" % 
-                            childPidCode).encode())
                 if childPidCode[0] == rc:
                     break
             except ChildProcessError:
-                print("child processes already terminated")
+                os.write(2, ("child processes already terminated").encode())
                 break
                 pass
             
